28082b2dd13c7e8cbfccfb35fb3dc03efd94ca2c
MHLinh <linhmaihoang2905@gmail.com>
2019-12-01 00:54:58 +0000

    Update eventdonate.php

--- public/event/eventdonate.php
+++ public/event/eventdonate.php
@@ -17,12 +17,12 @@
         <link rel="stylesheet" href="../nav.css">
 
         <title>Charity Link</title>
+        
+    </head>
+    <body>
         <div>
             <?php include '../nav.php'; ?>
         </div>
-    </head>
-    <body>
-
         <br/>
         
         <div class="container">
