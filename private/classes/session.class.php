f376cb253ea43900bd018c0b11f617f1db19d9c4
Ravi Ghaghada <41049832+RaviGhaghada@users.noreply.github.com>
2019-12-01 03:31:55 +0000

    Update session.class.php

--- private/classes/session.class.php
+++ private/classes/session.class.php
@@ -3,7 +3,7 @@
 class Session {
 
   public $user_id;
-  public $username;
+  public $user_name;
   private $last_login;
 
   public const MAX_LOGIN_AGE = 60*60*24; // 1 day
@@ -17,8 +17,8 @@ class Session {
     if($admin) {
       // prevent session fixation attacks
       session_regenerate_id();
-      $this->admin_id = $_SESSION['id'] = $admin->id;
-      $this->username = $_SESSION['username'] = $admin->username;
+      $this->user_id = $_SESSION['id'] = $admin->id;
+      $this->user_name = $_SESSION['user_name'] = $admin->user_name;
       $this->last_login = $_SESSION['last_login'] = time();
     }
     return true;
@@ -26,23 +26,23 @@ class Session {
 
   public function is_logged_in() {
     // return isset($this->admin_id);
-    return isset($this->admin_id) && $this->last_login_is_recent();
+    return isset($this->id) && $this->last_login_is_recent();
   }
 
   public function logout() {
-    unset($_SESSION['admin_id']);
-    unset($_SESSION['username']);
+    unset($_SESSION['id']);
+    unset($_SESSION['user_name']);
     unset($_SESSION['last_login']);
-    unset($this->admin_id);
-    unset($this->username);
+    unset($this->id);
+    unset($this->user_name);
     unset($this->last_login);
     return true;
   }
 
   private function check_stored_login() {
-    if(isset($_SESSION['admin_id'])) {
-      $this->admin_id = $_SESSION['admin_id'];
-      $this->username = $_SESSION['username'];
+    if(isset($_SESSION['id'])) {
+      $this->id = $_SESSION['id'];
+      $this->user_name = $_SESSION['user_name'];
       $this->last_login = $_SESSION['last_login'];
     }
   }
