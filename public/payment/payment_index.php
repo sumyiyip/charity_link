f97bc4a5c01e2fe9654ecff967ed55ba19da0838
Terry Phung <terry.phung@gmail.com>
2019-12-01 03:21:12 +0000

    template for payment

---
+++ public/payment/payment_index.php
@@ -0,0 +1,11 @@
+<?php 
+  $title = "Charity Link";
+  require("user_path.php");
+	// include the header
+  require("../../private/shared/header.php");
+
+?>
+
+<?php
+	require($home_dir . "../private/shared/footer.php");
+?>
