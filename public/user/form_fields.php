7d554721badd51f8f277476b8b724c27a03b454b
MHLinh <linhmaihoang2905@gmail.com>
2019-12-01 02:23:43 +0000

    Update form_fields.php

--- public/user/form_fields.php
+++ public/user/form_fields.php
@@ -4,26 +4,64 @@
 // or without first setting the necessary object
 ?>
 
-<dl>
-  <dt>User Name</dt>
-  <dd><input type="text" name="user_name" value="<?php echo h($user->user_name); ?>" /></dd>
-</dl>
-
-<dl>
-  <dt>Email Address</dt>
-  <dd><input type="text" name="email_add" value="<?php echo h($user->email_add); ?>" /></dd>
-</dl>
-
-<dl>
-  <dt>Phone Number</dt>
-  <dd><input type="text" name="phone_number" value="<?php echo h($user->phone_number); ?>" /></dd>
-</dl>
-
-<dl>
-  <dt>Password</dt>
-  <dd><input type="password" name="password" value="" /></dd>
-</dl>
-<dl>
-  <dt>Confirm Password</dt>
-  <dd><input type="password" name="confirm_password" value="" /></dd>
-</dl>
+<div class="container">
+            <div class="card">
+              <div class="card-body">
+                <p class="card-title" style="text-align:center; font-weight:bold; font-size: 3rem;"><?php echo ("Sign up") ?></p>
+                <div class="row" >
+                    <div class="col-md">
+                        <dl>
+                            <div class="row d-flex justify-content-center">
+                                    <div class="col-md-2">
+                                        <dt>User Name</dt>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <dd><input type="text" name="user_name" value="<?php echo h($user->user_name); ?>" /></dd>
+                                   </div>
+                            </div>     
+                        </dl>
+                        <dl>
+                            <div class="row d-flex justify-content-center">
+                                    <div class="col-md-2">
+                                        <dt>Email Address</dt>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <dd><input type="text" name="email_add" value="<?php echo h($user->email_add); ?>" /></dd>
+                                   </div>
+                            </div>     
+                        </dl>
+                        <dl>
+                            <div class="row d-flex justify-content-center">
+                                    <div class="col-md-2">
+                                        <dt>Phone Number</dt>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <dd><input type="text" name="phone_number" value="<?php echo h($user->phone_number); ?>" /></dd>
+                                   </div>
+                            </div>     
+                        </dl>
+                        <dl>
+                            <div class="row d-flex justify-content-center">
+                                    <div class="col-md-2">
+                                        <dt>Password</dt>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <dd><input type="password" name="password" value="" /></dd>
+                                   </div>
+                            </div>     
+                        </dl>
+                        <dl>
+                            <div class="row d-flex justify-content-center">
+                                    <div class="col-md-2">
+                                        <dt>Confirm Password</dt>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <dd><input type="password" name="confirm_password" value="" /></dd>
+                                   </div>
+                            </div>     
+                        </dl>
+                    </div>
+                  </div>
+                </div>
+    </div>
+</div>
